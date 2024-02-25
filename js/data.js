6746a13abc9f95f0a174231b2221eea9d3d8985b
FarhanFardid <farhanfardid009@gmail.com>
2024-02-25 16:18:38 +0600

    navbar working on

--- js/data.js
+++ js/data.js
@@ -1,1 +1,14 @@
 console.log("connected");
+
+const loadData = async () =>{
+    const url = 'https://www.thesportsdb.com/api/v1/json/3/search_all_teams.php?l=English%20Premier%20League'
+    const res = await fetch(url);
+    const data = await res.json();
+  displayData(data.teams);
+}
+
+ function displayData(data) {
+    // console.log(data);
+    const team = data.map(t => console.log(t) );
+ }
+loadData();
