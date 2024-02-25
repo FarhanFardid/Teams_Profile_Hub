126360ef3321cd853f5ecf4bcc0af334effcd8fa
FarhanFardid <farhanfardid009@gmail.com>
2024-02-25 20:43:13 +0600

    avbar updtaes, carousel updates and teams profile section modified

--- js/data.js
+++ js/data.js
@@ -13,14 +13,12 @@ const loadData = async () =>{
     const div = document.createElement('div');
     div.innerHTML =`
     <div href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow  hover:bg-gray-200 ">
-    <img class="h-72 w-full" src=${data.strTeamBanner} alt="Team banner">
-   
-    <div class="flex flex-col  justify-between p-4 leading-normal">
-
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Team details:${data.strDescriptionEN.slice(0, 500)}</p>
-       <div class="flex justify-around">
-      <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src=${data.strTeamBadge} alt="Team Badge">
-       <img class="h-64 w-64" src=${data.strTeamJersey} alt="Team Jersey">
+    <img class="h-52 lg:h-72 w-full" src=${data.strTeamBanner} alt="Team banner">
+    <div class="flex flex-col p-2 lg:p-4 ">
+        <p class="mb-3 font-semibold lg:text-xl  text-gray-900 dark:text-gray-400"><span class="font-bold text-blue-900">Team details:</span> ${data.strDescriptionEN.slice(0, 475)}</p>
+       <div class="flex justify-around lg:p-2">
+      <img class="object-cover  h-52 w-52 lg:h-72 lg:w-72  border border-slate-900 lg:p-2 p-1 rounded-md" src=${data.strTeamBadge} alt="Team Badge">
+       <img class="object-cover h-52 w-52 lg:h-72 lg:w-72 border border-slate-900 lg:p-2 p-1 rounded-md" src=${data.strTeamJersey} alt="Team Jersey">
   
        </div>
     </div>
