04af7adae75149b6726bd974a924bc3918749fcd
FarhanFardid <farhanfardid009@gmail.com>
2024-02-25 17:31:50 +0600

    individual team data loading and displaying in the UI

--- js/data.js
+++ js/data.js
@@ -4,11 +4,18 @@ const loadData = async () =>{
     const url = 'https://www.thesportsdb.com/api/v1/json/3/search_all_teams.php?l=English%20Premier%20League'
     const res = await fetch(url);
     const data = await res.json();
-  displayData(data.teams);
+  displayData(data.teams[0]);
 }
 
  function displayData(data) {
-    // console.log(data);
-    const team = data.map(t => console.log(t) );
+    console.log(data);
+    const container = document.getElementById("team_container");
+    const div = document.createElement('div');
+    div.innerHTML =`
+    <h2>Name: ${data.
+        strAlternate} </h2>
+`;
+ container.appendChild(div);
+    // const team = data.map(t => console.log(t) );
  }
 loadData();
